29beac781388e4eb9c3c3c79ff9cbc1d41979ae0
Thibault-be <thibault.dhooghe@gmail.com>
2023-11-08 15:20:55 +0100

    Dealer cards hidden until player ends move

--- 2.black-jack/blackjack.js
+++ 2.black-jack/blackjack.js
@@ -8,10 +8,12 @@ let cardsDeck = [
 const drawCardBtn = document.querySelector(".player-draw");
 const stopBtn = document.querySelector(".player-stop");
 const playerCards = document.querySelector(".player-cards");
-const computerCards = document.querySelector(".computer-cards");
+const playerValue = document.querySelector(".player-value")
+const dealerCards = document.querySelector(".dealer-cards");
+const dealerValue = document.querySelector(".dealer-value");
+dealerValue.setAttribute("style", "visibility: hidden")
 
-function drawCard(howMany, array){
-
+function drawCard(howMany, array, user){
   for (let i = 0; i < howMany; i++){
     let suit = Math.floor(Math.random()*4+1)-1;
 
@@ -28,13 +30,24 @@ function drawCard(howMany, array){
 
       //remove hand from deck
       cardsDeck[suit].splice(number,1)
+
+      //Add hand to HTML
+      let cardHTMLElement = document.createElement("div");
+      cardHTMLElement.textContent = drawnCard;
+      if(user ==="player"){
+        playerCards.appendChild(cardHTMLElement);
+      }else{
+        cardHTMLElement.setAttribute("style", "visibility: hidden")
+        dealerCards.appendChild(cardHTMLElement);
+      };
     };
   };
+  
   return array
 };
 
 
-function determineValueHand(array){
+function determineValueHand(array, user){
   let handValue = 0;
   for (let i = 0; i < array.length; i++){
     let cardValue = Array.from(array[i]);
@@ -46,33 +59,46 @@ function determineValueHand(array){
     };
     handValue += cardValue
   };
-  console.log(handValue)
+  if(handValue > 21){
+    alert("Bust! More than 21! dealer's turn")
+    drawCardBtn.setAttribute("disabled", "");
+    stopBtn.setAttribute("disabled", "");
+  }
+  if(user === "player"){
+    playerValue.textContent = handValue;
+  }else{
+    dealerValue.textContent = handValue;
+  }
   return handValue;
 };
 
+let playerHand = []
+let dealerHand = []
 
-let playerStartHand = []
-let dealerStartHand = []
-
-drawCard(2, playerStartHand);
-
-drawCard(2, dealerStartHand);
-
-let playerStartValue = determineValueHand(playerStartHand)
+drawCard(2, playerHand, "player");
+drawCard(2, dealerHand, "dealer");
 
 
-
+determineValueHand(playerHand, "player")
+determineValueHand(dealerHand, "dealer")
 
 
 drawCardBtn.addEventListener("click", () =>{
-  let newCard = drawCard(1, playerStartHand)
-  determineValueHand(newCard);
+  let newCard = drawCard(1, playerHand, "player")
+  determineValueHand(newCard, "player");
 })
-
 
 
 stopBtn.addEventListener("click", () =>{
   drawCardBtn.setAttribute("disabled", "");
+
+  dealerValue.setAttribute("style", "visibilty: visible");
+
+  let dealerChildren = Array.from(dealerCards.children)
+  dealerChildren.forEach(child =>{
+    child.setAttribute("style", "visibilty: visible")
+  }) 
+    
 });
 
 
